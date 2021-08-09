1f1adb459109b314602a34bbd7575be995c7b426
maxx <553789965@qq.com>
2021-08-09 15:25:03 +0800

    feat[sentry]:使用官方平台搭建

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* 点击自己的demo出错 */}
+        {/* <buttton onClick={methodDoesNotExist}>点击</buttton> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
